2b47a5ac5e3c0b7a215e102b863aef2d1a9029cd
Degisew <jegisew21@gmail.com>
2023-01-06 16:26:51 +0300

    fix rubocop linters issue

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require_relative './person'
 class Student < Person
   attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     @classroom = classroom
     super(age, name, parent_permission: parent_permission)
@@ -8,9 +9,9 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-  classroom.students.push(self) unless classroom.students.include?(self)
-    end
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
+
   def play_hooky
     '¯\(ツ)/'
   end
