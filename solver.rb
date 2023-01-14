601fb6f17833eefc7f973c053e10feda42325d0c
Degisew <jegisew21@gmail.com>
2023-01-14 11:32:11 +0300

    add FizzBuzz method

--- solver.rb
+++ solver.rb
@@ -9,4 +9,16 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num
+    end
+  end
 end
